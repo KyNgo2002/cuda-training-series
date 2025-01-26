ef6acfec4584277287ed18b05b4ef7e016bb368e
Ky <kyngo992@gmail.com>
2025-01-26 14:02:52 -0800

    hw1 progress

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <cuda_runtime.h>
 
 __global__ void hello(){
 
-  printf("Hello from block: %u, thread: %u\n", blockidx.x, threadidx.x);
+  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
 int main(){
 
   hello<<<2, 2>>>();
+  // Kernel launches are asynchronous to the host thread, meaning the host thread will not wait for completion of kernel before 
+  // proceeding to the next line of code.
+  // Calling "cudaDeviceSynchronize" synchronizes the host thread to kernels.
   cudaDeviceSynchronize();
-}
-
+  return 0;
+}l
